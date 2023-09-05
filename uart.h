6e1ac3c233f7b2f0ba4210c4252c4cfe9b50c747
Nissanth Karunanithy <99655617+Nissanthk@users.noreply.github.com>
2023-09-05 17:37:10 +0200

    uart.h

---
+++ uart.h
@@ -0,0 +1,10 @@
+#pragma once
+
+void uart_init( unsigned int ubrr );
+void uart_transmit( unsigned char data );
+unsigned char uart_receiver( void );
+void uart_print( void);
+void uart_tester(void);
+
+
+
