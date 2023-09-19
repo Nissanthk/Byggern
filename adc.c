83e81fefa3b13a67f2d3c550697e904d59784228
Nissanth Karunanithy <99655617+Nissanthk@users.noreply.github.com>
2023-09-19 18:54:41 +0200

    Update adc.c

--- adc.c
+++ adc.c
@@ -2,49 +2,49 @@
 #include "adc.h"
 #include <util/delay.h>
 
-#define BASE_ADDRESS_ADC 0x1400 
+#define BASE_ADDRESS_ADC 0x1400
 #define NUMBER_OF_CHANNELS 4
 #define F_CPU 4915200UL
 
 typedef enum{
-	ADC_A0, 
-	ADC_A1, 
-	ADC_UNUSED, 
-	ADC_POWER_DOWN, 
-	ADC_INHIBIT_CONVERSION, 
-	ADC_BIPOLAR, 
-	ADC_DIFFERENTIAL, 
+	ADC_A0,
+	ADC_A1,
+	ADC_UNUSED,
+	ADC_POWER_DOWN,
+	ADC_INHIBIT_CONVERSION,
+	ADC_BIPOLAR,
+	ADC_DIFFERENTIAL,
 	ADC_CONVERT_SINGLE
 } adc_config_t;
 
-static volatile unsigned char* const adc_base_address = (unsigned char*)0x1400; 
+static volatile unsigned char* const adc_base_address = (unsigned char*)0x1400;
 
 void adc_init(void) {
 	static const uint8_t shared_channel_config = (1<<ADC_CONVERT_SINGLE)|(1<<ADC_INHIBIT_CONVERSION);
 	
 	for(uint8_t channel = 0; channel < 4; channel++){
-		adc_base_address[channel] = shared_channel_config | channel; 
+		adc_base_address[channel] = shared_channel_config | channel;
 	}
 	
-	DDRD |= 1<<DDD4; 
+	DDRD |= 1<<DDD4;
 	
-	OCR3AL = 0; 
-	ICR3L = 1; 
-	TCCR3A |= (1<<COM3A1); 
-	TCCR3B |= (1<<CS30); 
+	OCR3AL = 0;
+	ICR3L = 1;
+	TCCR3A |= (1<<COM3A1);
+	TCCR3B |= (1<<CS30);
 	
-	TCCR3A |= (1<<WGM31); 
+	TCCR3A |= (1<<WGM31);
 	TCCR3B |= (1<<WGM33) | (1<<WGM32);
 	
-	DDRE &= ~(1<<DDE0); 
+	DDRE &= ~(1<<DDE0);
 }
 
 uint8_t adc_read(uint8_t channel){
-	adc_base_address[channel] = (1<<ADC_CONVERT_SINGLE) | channel; 
+	adc_base_address[channel] = (1<<ADC_CONVERT_SINGLE) | channel;
 	while(!(PINE & 1<<PINE0))
 	{
-		PINE; 
+		PINE;
 	}
 	
-	return adc_base_address[channel]; 
+	return adc_base_address[channel];
 }
